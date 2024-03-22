41412361988f21431f74cfc6886e60555146e084
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-03-22 22:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-//your JS code here. If required.
 document.getElementById("from").addEventListener("click", function() {
   var block_id = document.getElementById("block_id").value;
   var color_id = document.getElementById("color_id").value;
+  var grid_items = document.getElementsByClassName("grid-item");
   var block = document.getElementById(block_id);
-  block.style.backgroundColor = color_id;
-});
 
-document.getElementById("reset_button").addEventListener("click", function() {
-  var blocks = document.getElementsByClassName("grid-item");
-  for (var i = 0; i < blocks.length; i++) {
-    blocks[i].style.backgroundColor = "transparent";
+  for (var i = 0; i < grid_items.length; i++) {
+    grid_items[i].style.backgroundColor = "transparent";
   }
+
+  block.style.backgroundColor = color_id;
 });
